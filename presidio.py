c196ccf1a8f6f86daf9fe07809c7eda5dc0025b5
piotr-druzdzel <piotr.druzdzel@gmail.com>
2023-11-09 11:48:19 +0100

    Show identified PII values from Presidio

--- presidio.py
+++ presidio.py
@@ -1,8 +1,27 @@
 from presidio_analyzer import AnalyzerEngine
 
-text = "His name is Mr. Jones and his phone number is 212-555-5555"
+text = """
+Our sustainability publications on the internet:
+🔗 www.henkel.com/sustainability/reports
+Henkel; Namthip Muanthongthae via Getty 
+Images; Nils Hendrik Müller; Peter Rigaud; 
+Henkel app available for iOS and Android:
+RgStudio via Getty Images; Westend61 via  
+Getty Images; Yagnik Gorasiya via Shutterstock
+Translation +49 605 740 496
+Hmm misiuniek@gmail.com
+Best ice cream: Zielona 15, 26-620 Warszawa
+RWS Holdings PLC 
+Matthew Shoesmith, Revelation, Hilden
+Publication date of this report
+March 7, 2023 
+"""
 
 analyzer = AnalyzerEngine()
 analyzer_results = analyzer.analyze(text=text, language="en")
 
 print(analyzer_results)
+
+print()
+for pii in analyzer_results:
+    print(text[pii.start:pii.end])
